ad6d52ce790a12d1566578023ae885bd3d952cdd
Vincent <rumble.vecent@outlook.com>
2022-03-19 21:32:15 +0800

    Add sample application to test the whole framework.

---
+++ Tests/SampleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using Nebula.Core;
+using Nebula.Resource.Identifiers;
+
+namespace SampleApplication;
+
+public static class SampleApplication
+{
+    public static void Main(string[] arguments)
+    {
+        Domain.Current.Launch();
+        Domain.Current.Acquire<object>(WildcardIdentifier.Anything);
+    }
+}
