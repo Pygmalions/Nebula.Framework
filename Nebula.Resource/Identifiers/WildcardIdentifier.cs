7370915b7e83e2e470b42447c6643588b4194359
Vincent <rumble.vecent@outlook.com>
2022-03-19 21:31:51 +0800

    Add WildcardIdentifier to indicate an resource which does not need any identification to get.

---
+++ Nebula.Resource/Identifiers/WildcardIdentifier.cs
@@ -0,0 +1,9 @@
+﻿namespace Nebula.Resource.Identifiers;
+
+public class WildcardIdentifier : IIdentifier
+{
+    public static readonly WildcardIdentifier Anything = new();
+    
+    protected WildcardIdentifier()
+    {}
+}
