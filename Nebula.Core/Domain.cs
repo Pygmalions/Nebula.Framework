2c894822f8638a3f55185e5545c606757043b293
Vincent <rumble.vecent@outlook.com>
2022-03-19 20:17:35 +0800

    Add in-plugin source auto discovery function to Domain.

--- Nebula.Core/Domain.cs
+++ Nebula.Core/Domain.cs
@@ -1,4 +1,9 @@
-﻿using Nebula.Resource;
+﻿using System.Reflection;
+using Nebula.Exceptions;
+using Nebula.Extending;
+using Nebula.Resource;
+
+[assembly: PluginAssembly]
 
 namespace Nebula.Core;
 
@@ -8,7 +13,28 @@ public class Domain : Container
         new(() => new Domain(), LazyThreadSafetyMode.ExecutionAndPublication);
 
     public static Domain Current => SingletonInstance.Value;
-    
+
     private Domain()
-    {}
+    {
+        // In-assembly source auto discovery.
+        foreach (var (_, assembly) in PluginRegistry.FoundPlugins)
+        {
+            foreach (var candidate in assembly.GetTypes())
+            {
+                if (candidate.IsSubclassOf(typeof(Source)))
+                    continue;
+                var sourceAttribute = candidate.GetCustomAttribute<SourceAttribute>();
+                if (sourceAttribute == null)
+                    continue;
+                if (candidate.GetConstructor(Type.EmptyTypes) == null)
+                    throw new UserError($"Source {candidate.Name} is marked with {nameof(SourceAttribute)} " +
+                                        $"but does not have a no-argument constructor.");
+                var source = Activator.CreateInstance(candidate) as Source;
+                if (source == null)
+                    throw new RuntimeError(
+                        $"Failed to construct {candidate.Name} in in-assembly source auto discovery.");
+                AddSource(source);
+            }
+        }
+    }
 }
