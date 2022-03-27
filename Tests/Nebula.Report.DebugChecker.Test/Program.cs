9db5236f89b00ff3837c13d78bc187928e27deed
Vincent <rumble.vecent@outlook.com>
2022-03-27 14:44:07 +0800

    Add a sample console application to test the debug configuration checker.

---
+++ Tests/Nebula.Report.DebugChecker.Test/Program.cs
@@ -0,0 +1,6 @@
+﻿using Nebula.Reporting;
+
+// An exception will be thrown in Debug configuration.
+Report.Warning("Warning Occured", "Should be an exception in debug mode..").Handle();
+
+Console.WriteLine("In Release Configuration.");
