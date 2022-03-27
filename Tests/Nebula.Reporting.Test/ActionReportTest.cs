84b2c5163fb2f363478c104fac8ef7bd6b8c3956
Vincent <rumble.vecent@outlook.com>
2022-03-27 19:57:53 +0800

    Add tests for action report.

---
+++ Tests/Nebula.Reporting.Test/ActionReportTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Nebula.Reporting.Test;
+
+public class ActionReportTest
+{
+    [Test]
+    public void FinishTest()
+    {
+        var reported = false;
+        GlobalReporters.Error.Received += _ => reported = true;
+
+        Assert.Throws<ReportExceptionWrapper>(() => ActionReport.Begin()
+            .DoAction(() => throw new Exception())
+            .Finish());
+        
+        Assert.True(reported);
+    }
+    
+    [Test]
+    public void CaptureTest()
+    {
+        var reported = false;
+        GlobalReporters.Error.Received += _ => reported = true;
+
+        Assert.DoesNotThrow(() => ActionReport.Begin()
+            .DoAction(() => throw new Exception())
+            .Failed?.GloballyNotify(false));
+        
+        Assert.True(reported);
+    }
+}
